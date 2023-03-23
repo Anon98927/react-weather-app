9b173cee10ef83804bbd9002fc3ccedb51ff68b9
Anon98927 <chelle8788@gmail.com>
2023-03-23 13:33:50 -0400

    added github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        The project is created by Michelle and is{" "}
+        <a
+          href="https://github.com/Anon98927/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
