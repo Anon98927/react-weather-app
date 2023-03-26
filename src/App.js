bb9dbce7a9cf980c3d3742c914da8a4084377f1e
Anon98927 <chelle8788@gmail.com>
2023-03-26 06:12:14 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        The project is created by Michelle and is{" "}
-        <a
-          href="https://github.com/Anon98927/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div classname="container">
+        <h1>Weather App</h1>
+        <footer>
+          The project is created by Michelle and is{" "}
+          <a
+            href="https://github.com/Anon98927/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
