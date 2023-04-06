d77385f3e740d73fef09a594d212013b811142fd
Anon98927 <chelle8788@gmail.com>
2023-04-06 14:03:20 -0400

    added daily forecast and css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon.js";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          Thu <WeatherIcon /> 19 10
+          <div className="Forecast-day">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="Forecast-temp">
+            <span className="Forecast-max"></span>19°F |
+            <span className="Forecast-min"> 10°F</span>
+          </div>
         </div>
       </div>
     </div>
